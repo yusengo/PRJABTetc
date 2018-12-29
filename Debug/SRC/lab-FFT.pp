29d62d3df85a95e218bb1e79101a4d320747e788
yusengo <yusengo@163.com>
2018-12-29 20:02:30 +0800

    just test

---
+++ Debug/SRC/lab-FFT.pp
@@ -0,0 +1,59 @@
+# FIXED
+
+lab-FFT.obj: ../SRC/lab-FFT.c
+lab-FFT.obj: F:/DSPprj/lab47-FFT/INCLUDE/DSP2833x_Device.h
+lab-FFT.obj: F:/DSPprj/lab47-FFT/INCLUDE/DSP2833x_Adc.h
+lab-FFT.obj: F:/DSPprj/lab47-FFT/INCLUDE/DSP2833x_DevEmu.h
+lab-FFT.obj: F:/DSPprj/lab47-FFT/INCLUDE/DSP2833x_CpuTimers.h
+lab-FFT.obj: F:/DSPprj/lab47-FFT/INCLUDE/DSP2833x_ECan.h
+lab-FFT.obj: F:/DSPprj/lab47-FFT/INCLUDE/DSP2833x_ECap.h
+lab-FFT.obj: F:/DSPprj/lab47-FFT/INCLUDE/DSP2833x_DMA.h
+lab-FFT.obj: F:/DSPprj/lab47-FFT/INCLUDE/DSP2833x_EPwm.h
+lab-FFT.obj: F:/DSPprj/lab47-FFT/INCLUDE/DSP2833x_EQep.h
+lab-FFT.obj: F:/DSPprj/lab47-FFT/INCLUDE/DSP2833x_Gpio.h
+lab-FFT.obj: F:/DSPprj/lab47-FFT/INCLUDE/DSP2833x_I2c.h
+lab-FFT.obj: F:/DSPprj/lab47-FFT/INCLUDE/DSP2833x_McBSP.h
+lab-FFT.obj: F:/DSPprj/lab47-FFT/INCLUDE/DSP2833x_PieCtrl.h
+lab-FFT.obj: F:/DSPprj/lab47-FFT/INCLUDE/DSP2833x_PieVect.h
+lab-FFT.obj: F:/DSPprj/lab47-FFT/INCLUDE/DSP2833x_Spi.h
+lab-FFT.obj: F:/DSPprj/lab47-FFT/INCLUDE/DSP2833x_Sci.h
+lab-FFT.obj: F:/DSPprj/lab47-FFT/INCLUDE/DSP2833x_SysCtrl.h
+lab-FFT.obj: F:/DSPprj/lab47-FFT/INCLUDE/DSP2833x_XIntrupt.h
+lab-FFT.obj: F:/DSPprj/lab47-FFT/INCLUDE/DSP2833x_Xintf.h
+lab-FFT.obj: F:/DSPprj/lab47-FFT/INCLUDE/DSP2833x_Examples.h
+lab-FFT.obj: F:/DSPprj/lab47-FFT/INCLUDE/DSP2833x_GlobalPrototypes.h
+lab-FFT.obj: F:/DSPprj/lab47-FFT/INCLUDE/DSP2833x_ePwm_defines.h
+lab-FFT.obj: F:/DSPprj/lab47-FFT/INCLUDE/DSP2833x_Dma_defines.h
+lab-FFT.obj: F:/DSPprj/lab47-FFT/INCLUDE/DSP2833x_I2C_defines.h
+lab-FFT.obj: F:/DSPprj/lab47-FFT/INCLUDE/DSP2833x_DefaultISR.h
+lab-FFT.obj: D:/TICCS/ccsv6/tools/compiler/c2000_6.2.5/include/math.h
+lab-FFT.obj: D:/TICCS/ccsv6/tools/compiler/c2000_6.2.5/include/linkage.h
+
+../SRC/lab-FFT.c: 
+F:/DSPprj/lab47-FFT/INCLUDE/DSP2833x_Device.h: 
+F:/DSPprj/lab47-FFT/INCLUDE/DSP2833x_Adc.h: 
+F:/DSPprj/lab47-FFT/INCLUDE/DSP2833x_DevEmu.h: 
+F:/DSPprj/lab47-FFT/INCLUDE/DSP2833x_CpuTimers.h: 
+F:/DSPprj/lab47-FFT/INCLUDE/DSP2833x_ECan.h: 
+F:/DSPprj/lab47-FFT/INCLUDE/DSP2833x_ECap.h: 
+F:/DSPprj/lab47-FFT/INCLUDE/DSP2833x_DMA.h: 
+F:/DSPprj/lab47-FFT/INCLUDE/DSP2833x_EPwm.h: 
+F:/DSPprj/lab47-FFT/INCLUDE/DSP2833x_EQep.h: 
+F:/DSPprj/lab47-FFT/INCLUDE/DSP2833x_Gpio.h: 
+F:/DSPprj/lab47-FFT/INCLUDE/DSP2833x_I2c.h: 
+F:/DSPprj/lab47-FFT/INCLUDE/DSP2833x_McBSP.h: 
+F:/DSPprj/lab47-FFT/INCLUDE/DSP2833x_PieCtrl.h: 
+F:/DSPprj/lab47-FFT/INCLUDE/DSP2833x_PieVect.h: 
+F:/DSPprj/lab47-FFT/INCLUDE/DSP2833x_Spi.h: 
+F:/DSPprj/lab47-FFT/INCLUDE/DSP2833x_Sci.h: 
+F:/DSPprj/lab47-FFT/INCLUDE/DSP2833x_SysCtrl.h: 
+F:/DSPprj/lab47-FFT/INCLUDE/DSP2833x_XIntrupt.h: 
+F:/DSPprj/lab47-FFT/INCLUDE/DSP2833x_Xintf.h: 
+F:/DSPprj/lab47-FFT/INCLUDE/DSP2833x_Examples.h: 
+F:/DSPprj/lab47-FFT/INCLUDE/DSP2833x_GlobalPrototypes.h: 
+F:/DSPprj/lab47-FFT/INCLUDE/DSP2833x_ePwm_defines.h: 
+F:/DSPprj/lab47-FFT/INCLUDE/DSP2833x_Dma_defines.h: 
+F:/DSPprj/lab47-FFT/INCLUDE/DSP2833x_I2C_defines.h: 
+F:/DSPprj/lab47-FFT/INCLUDE/DSP2833x_DefaultISR.h: 
+D:/TICCS/ccsv6/tools/compiler/c2000_6.2.5/include/math.h: 
+D:/TICCS/ccsv6/tools/compiler/c2000_6.2.5/include/linkage.h: 
---
+++ Debug/SRC/subdir_rules.mk
@@ -0,0 +1,83 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+# Each subdirectory must supply rules for building sources it contributes
+DSP2833x_ADC_cal.obj: ../SRC/DSP2833x_ADC_cal.asm $(GEN_OPTS) $(GEN_HDRS)
+	@echo 'Building file: $<'
+	@echo 'Invoking: C2000 Compiler'
+	"D:/TICCS/ccsv6/tools/compiler/c2000_6.2.5/bin/cl2000" --silicon_version=28 -g --define="_DEBUG" --define="LARGE_MODEL" --include_path="F:/DSPprj/lab47-FFT/INCLUDE" --include_path="D:/TICCS/ccsv6/tools/compiler/c2000_6.2.5/include" --include_path="/packages/ti/xdais" --diag_warning=225 --large_memory_model --float_support=fpu32 --preproc_with_compile --preproc_dependency="SRC/DSP2833x_ADC_cal.pp" $(GEN_OPTS__FLAG) "$<"
+	@echo 'Finished building: $<'
+	@echo ' '
+
+DSP2833x_CpuTimers.obj: ../SRC/DSP2833x_CpuTimers.c $(GEN_OPTS) $(GEN_HDRS)
+	@echo 'Building file: $<'
+	@echo 'Invoking: C2000 Compiler'
+	"D:/TICCS/ccsv6/tools/compiler/c2000_6.2.5/bin/cl2000" --silicon_version=28 -g --define="_DEBUG" --define="LARGE_MODEL" --include_path="F:/DSPprj/lab47-FFT/INCLUDE" --include_path="D:/TICCS/ccsv6/tools/compiler/c2000_6.2.5/include" --include_path="/packages/ti/xdais" --diag_warning=225 --large_memory_model --float_support=fpu32 --preproc_with_compile --preproc_dependency="SRC/DSP2833x_CpuTimers.pp" $(GEN_OPTS__FLAG) "$<"
+	@echo 'Finished building: $<'
+	@echo ' '
+
+DSP2833x_DefaultIsr.obj: ../SRC/DSP2833x_DefaultIsr.c $(GEN_OPTS) $(GEN_HDRS)
+	@echo 'Building file: $<'
+	@echo 'Invoking: C2000 Compiler'
+	"D:/TICCS/ccsv6/tools/compiler/c2000_6.2.5/bin/cl2000" --silicon_version=28 -g --define="_DEBUG" --define="LARGE_MODEL" --include_path="F:/DSPprj/lab47-FFT/INCLUDE" --include_path="D:/TICCS/ccsv6/tools/compiler/c2000_6.2.5/include" --include_path="/packages/ti/xdais" --diag_warning=225 --large_memory_model --float_support=fpu32 --preproc_with_compile --preproc_dependency="SRC/DSP2833x_DefaultIsr.pp" $(GEN_OPTS__FLAG) "$<"
+	@echo 'Finished building: $<'
+	@echo ' '
+
+DSP2833x_GlobalVariableDefs.obj: ../SRC/DSP2833x_GlobalVariableDefs.c $(GEN_OPTS) $(GEN_HDRS)
+	@echo 'Building file: $<'
+	@echo 'Invoking: C2000 Compiler'
+	"D:/TICCS/ccsv6/tools/compiler/c2000_6.2.5/bin/cl2000" --silicon_version=28 -g --define="_DEBUG" --define="LARGE_MODEL" --include_path="F:/DSPprj/lab47-FFT/INCLUDE" --include_path="D:/TICCS/ccsv6/tools/compiler/c2000_6.2.5/include" --include_path="/packages/ti/xdais" --diag_warning=225 --large_memory_model --float_support=fpu32 --preproc_with_compile --preproc_dependency="SRC/DSP2833x_GlobalVariableDefs.pp" $(GEN_OPTS__FLAG) "$<"
+	@echo 'Finished building: $<'
+	@echo ' '
+
+DSP2833x_I2C.obj: ../SRC/DSP2833x_I2C.c $(GEN_OPTS) $(GEN_HDRS)
+	@echo 'Building file: $<'
+	@echo 'Invoking: C2000 Compiler'
+	"D:/TICCS/ccsv6/tools/compiler/c2000_6.2.5/bin/cl2000" --silicon_version=28 -g --define="_DEBUG" --define="LARGE_MODEL" --include_path="F:/DSPprj/lab47-FFT/INCLUDE" --include_path="D:/TICCS/ccsv6/tools/compiler/c2000_6.2.5/include" --include_path="/packages/ti/xdais" --diag_warning=225 --large_memory_model --float_support=fpu32 --preproc_with_compile --preproc_dependency="SRC/DSP2833x_I2C.pp" $(GEN_OPTS__FLAG) "$<"
+	@echo 'Finished building: $<'
+	@echo ' '
+
+DSP2833x_PieCtrl.obj: ../SRC/DSP2833x_PieCtrl.c $(GEN_OPTS) $(GEN_HDRS)
+	@echo 'Building file: $<'
+	@echo 'Invoking: C2000 Compiler'
+	"D:/TICCS/ccsv6/tools/compiler/c2000_6.2.5/bin/cl2000" --silicon_version=28 -g --define="_DEBUG" --define="LARGE_MODEL" --include_path="F:/DSPprj/lab47-FFT/INCLUDE" --include_path="D:/TICCS/ccsv6/tools/compiler/c2000_6.2.5/include" --include_path="/packages/ti/xdais" --diag_warning=225 --large_memory_model --float_support=fpu32 --preproc_with_compile --preproc_dependency="SRC/DSP2833x_PieCtrl.pp" $(GEN_OPTS__FLAG) "$<"
+	@echo 'Finished building: $<'
+	@echo ' '
+
+DSP2833x_PieVect.obj: ../SRC/DSP2833x_PieVect.c $(GEN_OPTS) $(GEN_HDRS)
+	@echo 'Building file: $<'
+	@echo 'Invoking: C2000 Compiler'
+	"D:/TICCS/ccsv6/tools/compiler/c2000_6.2.5/bin/cl2000" --silicon_version=28 -g --define="_DEBUG" --define="LARGE_MODEL" --include_path="F:/DSPprj/lab47-FFT/INCLUDE" --include_path="D:/TICCS/ccsv6/tools/compiler/c2000_6.2.5/include" --include_path="/packages/ti/xdais" --diag_warning=225 --large_memory_model --float_support=fpu32 --preproc_with_compile --preproc_dependency="SRC/DSP2833x_PieVect.pp" $(GEN_OPTS__FLAG) "$<"
+	@echo 'Finished building: $<'
+	@echo ' '
+
+DSP2833x_SysCtrl.obj: ../SRC/DSP2833x_SysCtrl.c $(GEN_OPTS) $(GEN_HDRS)
+	@echo 'Building file: $<'
+	@echo 'Invoking: C2000 Compiler'
+	"D:/TICCS/ccsv6/tools/compiler/c2000_6.2.5/bin/cl2000" --silicon_version=28 -g --define="_DEBUG" --define="LARGE_MODEL" --include_path="F:/DSPprj/lab47-FFT/INCLUDE" --include_path="D:/TICCS/ccsv6/tools/compiler/c2000_6.2.5/include" --include_path="/packages/ti/xdais" --diag_warning=225 --large_memory_model --float_support=fpu32 --preproc_with_compile --preproc_dependency="SRC/DSP2833x_SysCtrl.pp" $(GEN_OPTS__FLAG) "$<"
+	@echo 'Finished building: $<'
+	@echo ' '
+
+DSP2833x_Xintf.obj: ../SRC/DSP2833x_Xintf.c $(GEN_OPTS) $(GEN_HDRS)
+	@echo 'Building file: $<'
+	@echo 'Invoking: C2000 Compiler'
+	"D:/TICCS/ccsv6/tools/compiler/c2000_6.2.5/bin/cl2000" --silicon_version=28 -g --define="_DEBUG" --define="LARGE_MODEL" --include_path="F:/DSPprj/lab47-FFT/INCLUDE" --include_path="D:/TICCS/ccsv6/tools/compiler/c2000_6.2.5/include" --include_path="/packages/ti/xdais" --diag_warning=225 --large_memory_model --float_support=fpu32 --preproc_with_compile --preproc_dependency="SRC/DSP2833x_Xintf.pp" $(GEN_OPTS__FLAG) "$<"
+	@echo 'Finished building: $<'
+	@echo ' '
+
+DSP2833x_usDelay.obj: ../SRC/DSP2833x_usDelay.asm $(GEN_OPTS) $(GEN_HDRS)
+	@echo 'Building file: $<'
+	@echo 'Invoking: C2000 Compiler'
+	"D:/TICCS/ccsv6/tools/compiler/c2000_6.2.5/bin/cl2000" --silicon_version=28 -g --define="_DEBUG" --define="LARGE_MODEL" --include_path="F:/DSPprj/lab47-FFT/INCLUDE" --include_path="D:/TICCS/ccsv6/tools/compiler/c2000_6.2.5/include" --include_path="/packages/ti/xdais" --diag_warning=225 --large_memory_model --float_support=fpu32 --preproc_with_compile --preproc_dependency="SRC/DSP2833x_usDelay.pp" $(GEN_OPTS__FLAG) "$<"
+	@echo 'Finished building: $<'
+	@echo ' '
+
+lab-FFT.obj: ../SRC/lab-FFT.c $(GEN_OPTS) $(GEN_HDRS)
+	@echo 'Building file: $<'
+	@echo 'Invoking: C2000 Compiler'
+	"D:/TICCS/ccsv6/tools/compiler/c2000_6.2.5/bin/cl2000" --silicon_version=28 -g --define="_DEBUG" --define="LARGE_MODEL" --include_path="F:/DSPprj/lab47-FFT/INCLUDE" --include_path="D:/TICCS/ccsv6/tools/compiler/c2000_6.2.5/include" --include_path="/packages/ti/xdais" --diag_warning=225 --large_memory_model --float_support=fpu32 --preproc_with_compile --preproc_dependency="SRC/lab-FFT.pp" $(GEN_OPTS__FLAG) "$<"
+	@echo 'Finished building: $<'
+	@echo ' '
+
+
